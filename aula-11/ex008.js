006b8bd1926be0c24b364d78c7cc600d05a0d368
entony-katy <145465653+entony-katy@users.noreply.github.com>
2024-04-26 14:44:00 -0300

    adicionando a aula 11

---
+++ aula-11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`sua velocidade atual é de ${vel}`)
+if (vel >= 60 ) {
+    console.log('vc foi multado por execesso de velocidade')
+}
+console.log('dirija sempre com cuidado')
---
+++ aula-11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'brasil'
+console.log(`vc estar vivendo no ${país}`)
+if (país != 'brasil') {
+    console.log('você é estrangeiro')
+} else {
+    console.log('vc é brasileiro')
+}
