ccfdc1c86ff5c977db7855c7bba80c0ff086db86
entony-katy <145465653+entony-katy@users.noreply.github.com>
2024-05-12 11:58:29 -0300

    adicionando o ex 17 da aula 14

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,14 +1,32 @@
-function Calcular() {
-    var txtnumero = document.getElementById('txtn')
-    var dados = document.getElementById('dados')
-    var res = document.getElementById('res')
-    var c = 0
-    var soma = 0
-    var numero = Number(txtnumero.value)
-    res.innerHTML = '' // para quando alterar o valor adicionado, no ficar se repetindo varias vezes 
-    while (c <= 10) {
-        soma = c * numero 
-        res.innerHTML += `${numero} X ${c} = ${soma} <br> `
-        c++
+function calcular() {
+//pegando as variaveis
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('tabu')
+
+    if (num.value == 0 ) { //caso o input esteja vazia 
+        alert('Adicione um valor abaixo')
+    } else { // caso tenha um valor adicionado
+        
+        var c = 0 //crinando a variavel contador
+        n = Number(num.value)
+ // transformando o valor da variavel em tipo de Number
+        tab.innerHTML = ''
+ // deixando o valor da tabuada vazia
+        for ( c ; c <= 10; c++) {
+            var item = document.createElement('option') 
+// criando um opt dentro da tabuada 
+
+            item.value = `valor${c}` 
+// adicionando o valor para ser exebido para o PHP
+
+            var soma = c * n 
+//criando uma variavel que guarde a soma do multiplicação do numero recebido com o valor do contador
+
+            item.innerHTML += `${n} X ${c} = ${soma}` 
+// dando um valor de texto ao opt, que sera o seu texto de exibição
+
+            tab.appendChild(item)
+// adicionando o opt dentro da tabuada.
+        }
     }
 }
